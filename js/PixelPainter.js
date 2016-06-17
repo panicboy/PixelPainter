457820250162f90ca09b7ff06fbb45be3a5f0f2e
Vincent <vincentnewkirk@gmail.com>
2016-06-16 18:26:04 -1000

    all core functionality completed

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -2,7 +2,10 @@ var canvas = document.getElementById('pp-canvas');
 var colorArr = ['red', 'orange', 'yellow','green','blue','violet','magenta'];
 var colorPalette = document.getElementById('paletteDiv');
 var selectedColor = '';
-
+var clearDiv = document.getElementById('clearDiv');
+clearDiv.innerHTML = 'Clear';
+var eraseDiv = document.getElementById('eraseDiv');
+eraseDiv.innerHTML = 'Erase';
 
 for( var i = 1; i <= 25; i++ ){
   var newPixel = document.createElement('div');
@@ -33,4 +36,16 @@ function setColor(){
   this.style.backgroundColor = selectedColor;
 }
 
+colorPalette.appendChild(clearDiv);
+colorPalette.appendChild(eraseDiv);
 
+eraseDiv.addEventListener('click', function(){
+  selectedColor = 'white';
+})
+
+clearDiv.addEventListener('click',function(){
+  var allPixels = document.getElementsByClassName('pixels');
+  for(var i = 0; i < allPixels.length; i++){
+    allPixels[i].style.backgroundColor = 'white';
+  }
+})
