78cbb10139378987bc2053f96776ed69b7a619aa
Vincent <vincentnewkirk@gmail.com>
2016-06-17 08:48:22 -1000

    click and drag functionality working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,5 +1,9 @@
+var colorArr = ['FF0000','FF6666','FF9999','FFCCCC',
+'FF8000','FFB266','FFE5CC','FFFF00','FFFF66','FFFF99'
+,'FFFFCC','00CC00','33FF33','99FF99','0000CC','4C0099'
+,'7F00FF','9933FF','CC99FF','FF00FF','000000'
+,'404040','808080','C0C0C0','FFFFFF'];
 var canvas = document.getElementById('pp-canvas');
-var colorArr = ['FF0000','FF6666','FF9999','FFCCCC','FF8000','FFB266','FFE5CC','FFFF00','FFFF66','FFFF99','FFFFCC','00CC00','33FF33','99FF99','0000CC','4C0099','7F00FF','9933FF','CC99FF','FF00FF','000000','404040','808080','C0C0C0','FFFFFF'];
 var colorPalette = document.getElementById('paletteDiv');
 var selectedColor = '';
 
@@ -9,12 +13,37 @@ clearDiv.innerHTML = 'Clear';
 var eraseDiv = document.getElementById('eraseDiv');
 eraseDiv.innerHTML = 'Erase';
 
+var holdDown = false;
+
+function selectColor(){
+  selectedColor = this.style.backgroundColor
+}
+
+function setColor(){
+  this.style.backgroundColor = selectedColor;
+}
+
+
 for( var i = 1; i <= 400; i++ ){
   var newPixel = document.createElement('div');
   newPixel.className = 'pixels'
 
   canvas.appendChild(newPixel);
-  newPixel.addEventListener('mousedown', setColor);
+  //newPixel.addEventListener('mousedown', setColor);
+
+  //event listeners to change color when clicking
+  newPixel.addEventListener('mousedown', function(){
+    this.style.backgroundColor = selectedColor;
+    holdDown = true;
+  });
+  newPixel.addEventListener('mouseup', function(){
+    holdDown = false;
+  });
+  newPixel.addEventListener('mouseenter', function(){
+    if( holdDown === true ){
+      this.style.backgroundColor = selectedColor;
+    }
+  });
   if( i % 20 === 0 ){
     var lineBreak = document.createElement('br');
     canvas.appendChild(lineBreak);
@@ -31,16 +60,14 @@ for( var i = 0; i < colorArr.length; i++ ){
   colorPixel.style.backgroundColor = colorArr[i];
   colorPalette.appendChild(colorPixel);
   colorPixel.addEventListener('click', selectColor)
-
+  colorPixel.addEventListener('mouseenter', function(){
+    setInterval(function(){
+    this.style.width = (parseFloat(this.style.width) + 1) + 'px';
+    this.style.height = (parseFloat(this.style.height) + 1 ) + 'px';
+    }, 1000/30)
+  })
 }
 
-function selectColor(){
-  selectedColor = this.style.backgroundColor
-}
-
-function setColor(){
-  this.style.backgroundColor = selectedColor;
-}
 
 colorPalette.appendChild(clearDiv);
 colorPalette.appendChild(eraseDiv);
@@ -54,5 +81,11 @@ clearDiv.addEventListener('click',function(){
   // document.getElementsByClassName('pixels').setAttribute(backgroundColor, 'white');
   for(var i = 0; i < allPixels.length; i++){
     allPixels[i].style.backgroundColor = 'white';
+    holdDown = false;
   }
 })
+
+function makeItBigger(){
+    this.style.width = (parseFloat(this.style.width) + 1) + 'px';
+    this.style.height = (parseFloat(this.style.height) + 1 ) + 'px';
+  }
